bb2eaaf3df2f91898809da0d19b2307894055b01
SeeEun Cho <chwhse@gmail.com>
2017-03-21 10:40:38 +0900

    ㅎㅎ

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -18,23 +18,36 @@ public class Customer {
 	}
 	
 	public String statement(){	//대여료와 적립포인트 출력
-		double totalAmount = 0;	//총대여료
+		double totalAmount = 0;
 		int frequentRenterPoints = 0;	//적립포인트
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName()+"고객님의 대여기록 \n");
 		
 		for(Rental each:rentals){
-			frequentRenterPoints += each.getFreQuentRentalPoints();
-			
-			
+			frequentRenterPoints += getFreQuentRentalPoints();
 			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
-			
-			totalAmount += each.getCharge();
 		}	// for loop
 		
-		sb.append(String.format("누적 대여료 : %s%n적립 포인트: %s%n", totalAmount, frequentRenterPoints));
+		sb.append(String.format("누적 대여료 : %s%n적립 포인트: %s%n", getTotalCharge(), frequentRenterPoints));
 		
 		return sb.toString();
+	}
+
+	private int getFreQuentRentalPoints() {
+		int result = 0;
+		for(Rental each:rentals){
+			result += each.getCharge();
+		}
+		return result;
+	}
+
+	private Object getTotalCharge() {
+		double result = 0;
+		for(Rental each:rentals){
+			result += each.getCharge();
+		}
+		return result;
+		
 	}
 
 	
